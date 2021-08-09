5347bc87584ce6efe535f5ab671e01b9c820ab77
Baran Önen <baranonenn@gmail.com>
2021-08-10 00:20:07 +0300

    Fixes

--- main.js
+++ main.js
@@ -8,11 +8,7 @@ function loaded() {
 }
 
 function keyCheck() {
-	if (window.localStorage.getItem('areKeysAdded') != "true") {
-		document.getElementById("keyError").style.display = "block"
-		document.getElementById("sendButton").disabled = true
-		document.getElementById("tweetInput").disabled = true
-	} else {
+	if (localStorage.getItem('consumerKey') && localStorage.getItem('consumerSecret') && localStorage.getItem('accessToken') && localStorage.getItem('accessTokenSecret')) {
 		document.getElementById("keyError").style.display = "none"
 		document.getElementById("sendButton").disabled = false
 		document.getElementById("tweetInput").disabled = false
@@ -20,15 +16,19 @@ function keyCheck() {
 		document.getElementById("consumerSecret").value = localStorage.getItem("consumerSecret")
 		document.getElementById("accessToken").value = localStorage.getItem("accessToken")
 		document.getElementById("accessTokenSecret").value = localStorage.getItem("accessTokenSecret")
+		T = new Twit({
+			consumer_key:         localStorage.getItem("consumerKey"),
+			consumer_secret:      localStorage.getItem("consumerSecret"),
+			access_token:         localStorage.getItem("accessToken"),
+			access_token_secret:  localStorage.getItem("accessTokenSecret"),
+			timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+			strictSSL:            true,     // optional - requires SSL certificates to be valid.
+		  })
+	} else {
+		document.getElementById("keyError").style.display = "block"
+		document.getElementById("sendButton").disabled = true
+		document.getElementById("tweetInput").disabled = true
 	}
-	T = new Twit({
-		consumer_key:         localStorage.getItem("consumerKey"),
-		consumer_secret:      localStorage.getItem("consumerSecret"),
-		access_token:         localStorage.getItem("accessToken"),
-		access_token_secret:  localStorage.getItem("accessTokenSecret"),
-		timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-		strictSSL:            true,     // optional - requires SSL certificates to be valid.
-	  })
 }
 
 function openSettings() {
